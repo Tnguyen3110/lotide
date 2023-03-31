d0d5b6e7ec005050433dd3cd19cd538f16758051
Tra Nguyen <ntptra85@gmail.com>
2023-03-31 12:08:44 -0600

    initial commit of countOnly

--- countOnly.js
+++ countOnly.js
@@ -10,7 +10,13 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     if(itemsToCount[item]) {
-      results[item] ? results[item] += 1 : results[item] = 1
+      if (results[item]) {
+        results[item] += 1
+      }
+      else {
+        results[item] = 1
+      }
+      
     }
   }
    
